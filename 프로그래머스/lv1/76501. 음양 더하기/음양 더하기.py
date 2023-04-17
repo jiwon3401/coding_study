20d3eabd7b6c090d28b36c6b9fc2efe8ce4776d1
Jiwon Park <85453818+jiwon3401@users.noreply.github.com>
2023-04-17 15:46:42 +0900

    [level 1] Title: 음양 더하기, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(signs)):
+        if signs[i]==True:
+            answer += absolutes[i]*1
+        else:
+            answer-= absolutes[i]
+    return answer
