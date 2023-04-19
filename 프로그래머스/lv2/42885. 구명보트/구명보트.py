2f3821b5ca888edc1e5bf7c3e0a87b541676dad3
jiwon <jiwon3401@gmail.com>
2023-04-19 11:13:21 +0900

    update

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -17,3 +17,18 @@ def solution(people, limit):
             w.pop()
             answer += 1
     return answer
+
+###############################################
+#다른사람풀이 -> return값에 바로 (전체인원-짝지은수)=보트수 
+def solution(people, limit) :
+    answer = 0
+    people.sort()
+
+    a = 0
+    b = len(people) - 1
+    while a < b :
+        if people[b] + people[a] <= limit :
+            a += 1
+            answer += 1
+        b -= 1
+    return len(people) - answer
