b7da0b91e373f1e167657d4a97aa84a8f6345e74
Jiwon Park <85453818+jiwon3401@users.noreply.github.com>
2023-04-22 19:31:53 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.90 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+def solution(numbers):
+    answer = []
+    comb = list(combinations(numbers,2))
+    for i in comb:
+        answer.append(i[0]+i[1])
+    answer.sort()
+    ans = []
+    for i in answer:
+        if i not in ans:
+            ans.append(i)
+    return ans
