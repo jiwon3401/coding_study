9383cdaa3f866988ceec54704f9c95341b9b31e5
jiwon <jiwon3401@gmail.com>
2023-05-19 18:44:12 +0900

    update

---
+++ algorithm/sec8/Q2_sol.py
@@ -0,0 +1,16 @@
+import sys
+#sys.stdin = open("input.txt",'r')
+
+def DFS(len):
+    if dy[len]>0: #시간복잡도 줄이기
+        return dy[len]
+    if len==1 or len==2:
+        return len
+    else:
+        dy[len]=DFS(len-1)+DFS(len-2)
+        return dy[len]
+    
+if __name__=="__main__":
+    n=int(input())
+    dy=[0]*(n+1)
+    print(DFS(n))
