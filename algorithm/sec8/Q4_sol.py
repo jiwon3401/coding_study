1ce68a73171d4f91161099608a3bfcbc34e3cde1
jiwon <jiwon3401@gmail.com>
2023-05-22 15:51:11 +0900

    update

--- algorithm/sec8/Q4_sol.py
+++ algorithm/sec8/Q4_sol.py
@@ -8,4 +8,11 @@ dy=[0]*(n+1)
 dy[1]=1
 res=0
 for i in range(2,n+1):
-    
+    max=0
+    for j in range(i-1,0,-1):
+        if arr[j]<arr[i] and dy[j]>max: #dy[j]: arr[j]가 마지막 항일 때 최대 수열 길이를 의미
+            max=dy[j]
+    dy[i]=max+1
+    if dy[i]>res: #최대값 찾기
+        res=dy[i]
+print(res)
