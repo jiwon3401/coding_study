20d896235d4c8293316feccca29b3bd3f41b5bc1
Jiwon Park <85453818+jiwon3401@users.noreply.github.com>
2023-06-23 00:24:28 +0900

    [unrated] Title: 코드 처리하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,18 @@
+def solution(code):
+    ret = ''
+    mode= 0
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i]!='1':
+                if i%2==0:
+                    ret+=code[i]
+            else:
+                mode=1
+        else:   
+            if code[i]!='1':
+                if i%2!=0:
+                    ret+=code[i]
+            else:
+                mode=0
+    
+    return ret if ret else 'EMPTY'
